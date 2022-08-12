c833c86ec0c0c660a79bd0b1b3b4a5f1634d3589
damocles217 <damm084@tutanota.com>
2022-08-12 01:40:40 -0500

    Fixed errors in css

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,13 @@
 import React, { ComponentType, lazy } from 'react';
 const Header = lazy(() => import('./Header/Header'));
 import style from './App.module.scss';
+import Chat from './Chat/Chat';
 
 const App: ComponentType = () => {
   return (
     <div className={style.container}>
       <Header />
+      <Chat />
     </div>
   );
 };
